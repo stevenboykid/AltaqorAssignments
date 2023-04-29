7bff82d5341ca28f9686f751da5bc6dfd56d1116
stevenboykid <pahari.rc2010@gmail.com>
2023-04-29 10:04:07 -0500

    Assignment07

---
+++ src/altaqor/assignments/assignment7/Car.java
@@ -0,0 +1,15 @@
+package altaqor.assignments.assignment7;
+
+public class Car {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Make :Toyota");
+		System.out.println("Model : Rav4");
+		System.out.println("Year : 2019");
+		System.out.println("Color : Silver");
+		System.out.println("make Toyota\n Model Rav4\n Year 2019\n Color Silver");
+	}
+	
+
+}
