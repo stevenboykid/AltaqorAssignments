1aacf61c2acddcc2af34d18b91e34ebfde49233c
stevenboykid <pahari.rc2010@gmail.com>
2023-05-12 01:52:22 -0500

    assignment 10,11,12)

---
+++ src/altaqor/assignments/assignment12/CheckPositiveAndnegitive.java
@@ -0,0 +1,21 @@
+package altaqor.assignments.assignment12;
+
+public class CheckPositiveAndnegitive {
+ public static void main(String[] args) {
+	int x=0;
+	 boolean pCheck= x>0;
+	 boolean nCheck= x<0;
+	 boolean zCheck=x==0;
+	if (pCheck) {
+		System.out.println("positive Number");
+		
+	}
+	if (nCheck) {
+		System.out.println("Negative Numver");
+	}
+	if (zCheck) {
+		System.out.println("zero");
+	}
+}
+ 
+}
--- src/altaqor/assignments/assignment12/EligibleForVote.java
+++ src/altaqor/assignments/assignment12/EligibleForVote.java
@@ -3,13 +3,13 @@ package altaqor.assignments.assignment12;
 public class EligibleForVote {
 	public static void main(String[] args) {
 		
-		int myAge =40;
+		int myAge =16;
 		
 		if (myAge<18) {
 			System.out.println("You are not eligible for vote as seems Minor");
 		}
 		
-		else{
+		if (myAge>=18){
 			System.out.println("You are eligible for vote");
 		}
 		
